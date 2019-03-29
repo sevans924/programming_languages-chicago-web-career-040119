2c45025e0735f808d7d8f41ff4b1859cbe74cd96
Sarah Elzinga <sevans924@gmail.com>
2019-03-29 14:27:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
+
+
+
 def reformat_languages(languages)
+  new_hash = {}
+languages.each do |oo_functional, language_hash|
+
+  language_hash.each do |language, attribute_hash|
+
+    attribute_hash.each do |attribute, value|
+
+     if new_hash[language].nil?
+
+        new_hash[language] = {}
+      end
+      new_hash[language][:style] = []
+      new_hash[language][:style] << oo_functional
+    #  if new_hash[language][attribute].nil?
+        #new_hash[language][attribute] = str_value
+      #end
+
+    end
+  end
+end
   # your code here
+  new_hash
 end
